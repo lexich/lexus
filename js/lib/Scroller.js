c26e664f78b18fedb7ea02f4ff43d0fd3f540b87
lexich <lexich121@gmail.com>
2013-06-03 01:08:27 +0400

    update:

--- js/lib/Scroller.js
+++ js/lib/Scroller.js
@@ -5,6 +5,7 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
   Scroller = (function(__super__) {
     return __super__.extend({
       el: "[data-js-scrollmenu]",
+      $panel: $(),
       events: {
         "click a[href^='#']": "event_click",
         "click a[href^='#'] > *": "event_click_hack"
@@ -14,11 +15,17 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
       parralax: {},
       parralaxItems: $(),
       initialize: function() {
-        var $links, prev,
+        $(window).scroll(_.bind(this.event_windowScroll, this));
+        $(window).resize(_.bind(this.event_windowResize, this));
+        this.$panel = $("[data-js-scrollmenu-panel]", this.$el);
+        this.event_windowResize();
+        return this.autoScroll();
+      },
+      event_windowResize: function() {
+        var $links, normHeigth, prev,
           _this = this;
 
-        $(window).scroll(_.bind(this.event_windowScroll, this));
-        this.autoScroll();
+        this.parralax = {};
         $links = _.chain($("a[href^='#']", this.$el)).uniq(function(item) {
           return $(item).attr("href");
         }).sortBy(function(item) {
@@ -28,41 +35,43 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
           return $(href).position().top;
         }).value();
         prev = 0;
+        normHeigth = $(document).height() / (1.0 + $links.length);
         _.each($links, function(link) {
-          var from, to;
+          var $anchor, from, to, _ref;
 
-          to = _this.getAnchorPos($(link).attr("href"));
+          _ref = _this.getAnchorPos($(link).attr("href")), to = _ref[0], $anchor = _ref[1];
           from = parseInt(prev);
           to = parseInt(to);
           _this.parralax[to] = {
             from: from,
             to: to,
+            move: to - from,
             call: function(p) {
               var x;
 
-              x = Math.PI * (p - this.from) / (this.to - this.from);
-              return Math.sin(x);
+              x = Math.PI * (p - this.from) / this.move;
+              return (this.move / normHeigth) * Math.sin(x);
             }
           };
           return prev = to;
         });
         this.parralaxItems = $("[data-parralax]");
-        return _.each(this.parralaxItems, function(item) {
+        _.each(this.parralaxItems, function(item) {
           var mTop;
 
           mTop = parseInt($(item).css("marginTop").replace("px", ""));
           return $(item).data('parralax-top', mTop);
         });
+        return this.autoScroll();
       },
-      scroll: function(from, to, duration) {
-        var move,
+      scroll: function(options) {
+        var $anchor, duration, from, menuFrom, menuMove, menuTo, move, to, _ref,
           _this = this;
 
-        if (duration == null) {
-          duration = 2000;
-        }
-        from = parseInt(from);
-        to = parseInt(to);
+        from = parseInt(options.from);
+        to = parseInt(options.to);
+        duration = (_ref = options.duration) != null ? _ref : 2000;
+        $anchor = options.$anchor;
         _this = this;
         move = to - from;
         if (Math.abs(move) < EPS) {
@@ -70,41 +79,62 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
         }
         this.$el.stop(true, false);
         this.$el.prop("scroll", from);
+        menuFrom = this.getMenuCurPos();
+        menuTo = this.getMenuPosItem(to, $anchor);
+        menuMove = menuTo - menuFrom;
+        this.$el.prop("menuMargin", menuFrom);
         return this.$el.animate({
-          scroll: "+=" + move
+          scroll: "+=" + move,
+          menuMargin: "+=" + menuMove
         }, {
           duration: duration,
-          step: function(now) {
-            _this.stepAnimation += 1;
-            return window.scrollTo(0, now);
+          step: function(now, tween) {
+            if (tween.prop === "scroll") {
+              _this.stepAnimation += 1;
+              return window.scrollTo(0, now);
+            } else if (tween.prop === "menuMargin") {
+              return _this.$panel.css("marginTop", "" + now + "px");
+            }
           },
           complete: function() {}
         });
       },
       autoScroll: function() {
-        var from, move,
+        var $anchor, duration, from, marginTop, move, to,
           _this = this;
 
         from = from = $(window).scrollTop();
+        $anchor = $();
         move = _.reduce(this.$el.find("a[href^='#']"), (function(memo, a) {
-          var delta, href, top;
+          var $_anchor, delta, href, top, _ref;
 
           href = $(a).attr("href");
-          top = _this.getAnchorPos(href);
+          _ref = _this.getAnchorPos(href), top = _ref[0], $_anchor = _ref[1];
           if (top == null) {
             return memo;
           }
           delta = top - from;
           if (Math.abs(delta) < Math.abs(memo)) {
+            $anchor = $_anchor;
             return delta;
           } else {
             return memo;
           }
         }), $(document).height());
-        if (Math.abs(move) < EPS) {
-          return;
+        if (!(Math.abs(move) < EPS)) {
+          to = from + move;
+          duration = 1000;
+          return this.scroll({
+            from: from,
+            to: to,
+            duration: duration,
+            $anchor: $anchor
+          });
+        } else {
+          to = this.getAnchorPosItem($anchor);
+          marginTop = this.getMenuPosItem(to, $anchor);
+          return this.$panel.css("marginTop", "" + marginTop + "px");
         }
-        return this.scroll(from, from + move, 1000);
       },
       event_windowScroll: function(e) {
         var eff, i, mapKey, now, val, _i, _len, _ref,
@@ -150,19 +180,37 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
         return this.delay = setTimeout((function() {
           _this.autoScroll();
           return _this.delay = null;
-        }), 500);
+        }), 1000);
+      },
+      getMenuCurPos: function() {
+        var marginTop;
+
+        marginTop = parseFloat(this.$panel.css("marginTop").replace("px", ""));
+        return marginTop;
+      },
+      getMenuPosItem: function(pos, $anchor) {
+        var offset, top;
+
+        top = $anchor.offset().top;
+        offset = 18;
+        if (top < pos) {
+          return offset;
+        } else {
+          return (top - pos) + offset;
+        }
       },
       getAnchorPos: function(href) {
-        var $anchor;
+        var $anchor, pos;
 
         if (!/^#.+/.test(href)) {
-          return;
+          return [null, $anchor];
         }
         $anchor = $(href);
         if ($anchor.length !== 1) {
-          return;
+          return [null, $anchor];
         }
-        return this.getAnchorPosItem($anchor);
+        pos = this.getAnchorPosItem($anchor);
+        return [pos, $anchor];
       },
       getAnchorPosItem: function($anchor) {
         var heigth, move, result, top, wHeigth;
@@ -189,15 +237,19 @@ define(["jQuery", "underscore", "Backbone"], function($, _, Backbone) {
         return this.click_link($(e.target));
       },
       click_link: function($link) {
-        var from, href, to;
+        var $anchor, from, href, to, _ref;
 
         href = $link.attr("href");
         from = $(window).scrollTop();
-        to = this.getAnchorPos(href);
+        _ref = this.getAnchorPos(href), to = _ref[0], $anchor = _ref[1];
         if (to == null) {
           return;
         }
-        return this.scroll(from, to);
+        return this.scroll({
+          from: from,
+          to: to,
+          $anchor: $anchor
+        });
       }
     });
   })(Backbone.View);
